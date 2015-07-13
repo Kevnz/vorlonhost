82c239260320dc787695682cc11e7910a31c6690
Kevin Isom <kevin.isom@gmail.com>
2015-07-13 17:02:54 +1200

    make it work

--- vorlon/Server/config/vorlon.httpconfig.js
+++ vorlon/Server/config/vorlon.httpconfig.js
@@ -6,16 +6,18 @@ var VORLON;
 (function (VORLON) {
     var HttpConfig = (function () {
         function HttpConfig() {
+            console.log(path.join(__dirname, "../config.json"));
             var catalogdata = fs.readFileSync(path.join(__dirname, "../config.json"), "utf8");
             var catalogstring = catalogdata.toString().replace(/^\uFEFF/, '');
             var catalog = JSON.parse(catalogstring);
+            console.log(catalog);
             if (catalog.useSSL) {
                 this.useSSL = true;
                 this.protocol = "https";
                 this.httpModule = https;
                 this.options = {
-                    key: fs.readFileSync(catalog.SSLkey),
-                    cert: fs.readFileSync(catalog.SSLcert)
+                    key: fs.readFileSync(path.join(__dirname,catalog.SSLkey)),
+                    cert: fs.readFileSync(path.join(__dirname,catalog.SSLcert))
                 };
             }
             else {
